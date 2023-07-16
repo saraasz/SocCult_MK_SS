154aee867a02ccbac534cebe814e1ffe44b20e45
Márton Kardos <power.up1163@gmail.com>
2023-07-16 14:27:56 +0200

    Added regression metrics and MCMC diagnostics

---
+++ evaluate_convergence.py
@@ -0,0 +1,47 @@
+import glob
+from pathlib import Path
+from typing import Callable
+
+import arviz as az
+import numpy as np
+import pandas as pd
+from tqdm import tqdm
+
+
+def run_mcmc_diagnostic(
+    simulation_results: list[dict], diagnostic: Callable
+) -> pd.DataFrame:
+    records = []
+    for sim in tqdm(simulation_results, desc=diagnostic.__name__):
+        idata = az.convert_to_inference_data({"elections": sim["elections"]})
+        results = diagnostic(idata)
+        results = results["elections"].to_numpy()
+        record = {
+            "certainty": float(sim["certainty"]),
+            "faithfulness": float(sim["faithfulness"]),
+            "tactic": float(sim["tactic"]),
+        }
+        for i_candidate, value in enumerate(results):
+            record[f"{diagnostic.__name__}_cand_{i_candidate}"] = value
+    return pd.DataFrame.from_records(records)
+
+
+def main() -> None:
+    print("Loading files")
+    files = glob.glob("simulations/sim*.npz")
+    simulation_results = [dict(np.load(file)) for file in files]
+
+    # Creating output dir
+    out_dir = Path("results")
+    out_dir.mkdir(exist_ok=True)
+
+    print("Running MCMC diagnostics")
+    ess_res = run_mcmc_diagnostic(simulation_results, diagnostic=az.ess)
+    ess_res.to_feather(out_dir.joinpath("ess.feather"))
+    rhat_res = run_mcmc_diagnostic(simulation_results, diagnostic=az.rhat)
+    rhat_res.to_feather(out_dir.joinpath("rhat.feather"))
+    print("DONE")
+
+
+if __name__ == "__main__":
+    main()
---
+++ evaluate_regression_metrics.py
@@ -0,0 +1,69 @@
+import glob
+from pathlib import Path
+from typing import Callable
+
+import numpy as np
+import pandas as pd
+from sklearn.metrics import (max_error, mean_absolute_error,
+                             mean_absolute_percentage_error,
+                             mean_squared_error, r2_score)
+from tqdm import tqdm
+
+
+def evaluate_results(
+    simulation_results: list[dict],
+    ideal_results: np.ndarray,
+    metrics: list[Callable],
+) -> pd.DataFrame:
+    records = []
+    for sim in tqdm(simulation_results):
+        for election_chain in sim["elections"]:
+            for election in election_chain:
+                record = {
+                    "certainty": float(sim["certainty"]),
+                    "faithfulness": float(sim["faithfulness"]),
+                    "tactic": float(sim["tactic"]),
+                }
+                for metric in metrics:
+                    record[metric.__name__] = metric(ideal_results, election)
+                records.append(record)
+    return pd.DataFrame.from_records(records)
+
+
+def main() -> None:
+    print("Loading files")
+    init = np.load("simulations/init.npz")
+    files = glob.glob("simulations/sim*.npz")
+    simulation_results = [dict(np.load(file)) for file in files]
+
+    print("Calculating ideal results")
+    similarities = init["similarities"]
+    vote = init["similarities"].argmax(axis=1)
+    votes = np.zeros_like(similarities)
+    for i_voter, i_vote in enumerate(vote):
+        votes[i_voter, i_vote] = 1
+    ideal_results = votes.sum(axis=0)
+
+    print("Evaluating simulation results.")
+    regression_metrics = evaluate_results(
+        simulation_results,
+        ideal_results,
+        metrics=[
+            r2_score,
+            max_error,
+            mean_absolute_error,
+            mean_absolute_percentage_error,
+            mean_squared_error,
+        ],
+    )
+
+    print("Saving results")
+    out_dir = Path("results")
+    out_dir.mkdir(exist_ok=True)
+    regression_metrics.to_feather(
+        out_dir.joinpath("regression_metrics.feather")
+    )
+
+
+if __name__ == "__main__":
+    main()
